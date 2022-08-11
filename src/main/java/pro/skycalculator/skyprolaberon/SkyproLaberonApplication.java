1248cf59a4eabcec9227139ae6e11429485f1de4
Laberon <laberon83@gmail.com>
2022-08-11 17:36:33 +0300

    start

---
+++ src/main/java/pro/skycalculator/skyprolaberon/SkyproLaberonApplication.java
@@ -0,0 +1,13 @@
+package pro.skycalculator.skyprolaberon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproLaberonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproLaberonApplication.class, args);
+	}
+
+}
